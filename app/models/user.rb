d6cd697f532f4b7267dd3bfc88ec2b726d5fce4e
ryota0603 <114867300+ryota0603@users.noreply.github.com>
2022-12-02 16:07:09 +0000

    follow,view

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,13 @@ class User < ApplicationRecord
   has_many :books
   has_many :favorites, dependent: :destroy 
   has_many :book_comments, dependent: :destroy
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy 
+  # フォローする機能
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+　# フォローされた機能
+# 一覧画面で使う
+  has_many :followings, through: :relationships, source: :followed
+　has_many :followers, through: :reverse_of_relationships, source: :follower
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
@@ -16,5 +23,18 @@ class User < ApplicationRecord
   
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+  
+  # フォローしたときの処理
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  # フォローを外すときの処理
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  # フォローしているか判定
+  def following?(user)
+    followings.include?(user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "home/about"=>"homes#about" , as: 'about'
-  
+#   イイね機能
   resources :books do
    resource :favorites, only: [:create, :destroy]
    resources :book_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update]
+#  フォロー機能 
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
