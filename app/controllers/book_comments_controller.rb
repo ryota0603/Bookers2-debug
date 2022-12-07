651f5c0cf7c468861a4babf10d7b480f6dfd567a
ryota0603 <114867300+ryota0603@users.noreply.github.com>
2022-12-07 10:26:53 +0000

    comment,count

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -3,16 +3,21 @@ class BookCommentsController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    render  :book_comments
+    @books= Book.find(params[:book_id])
+    @book_comment = BookComment.new
+    @book_comments = BookComment.all
+    render  "book_comments"
   end
   
   def destroy
     @book = Book.find(params[:book_id])
-    book_comment = BookComment.find(params[:id])
-    book_comment.destroy
-    render :book_comments,  notice: "successfully delete book!"
+    @book_comment = BookComment.find(params[:id])
+    @book_comment.destroy
+    @book_comment = BookComment.new
+    @book_comments = BookComment.all
+    render  "book_comments"
   end
 
   private
